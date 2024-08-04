de438caf411beb1283c990a9df309bc01fdb23e8
Dinamiet <iceman.sm39@gmail.com>
2024-08-04 11:41:40 +0200

    Documented

--- include/quaternion.h
+++ include/quaternion.h
@@ -1,18 +1,62 @@
 #ifndef _QUATERNION_H_
 #define _QUATERNION_H_
 
+/**
+ * \file
+ *
+ * Quaternion
+ *
+ * Quaternion library
+ */
+
 #include "vector.h"
 
+/**
+ * Quaternion structure
+ */
 typedef struct _Quaternion_
 {
-	float  R;
-	Vector V;
+	float  R; /** Quaternion R component */
+	Vector V; /** Quaternion Vector component */
 } Quaternion;
 
+/**
+ * Create a Quaternion from a vector and angle
+ * \param v Vector
+ * \param theta Angle
+ * \return New Quaternion
+ */
 Quaternion Quaternion_Create(Vector v, float theta);
+
+/**
+ * Add two quaternions together
+ * \param a Quaternion A
+ * \param b Quaternion B
+ * \return A + B
+ */
 Quaternion Quaternion_Add(const Quaternion a, const Quaternion b);
+
+/**
+ * Multiply quaternions
+ * \param a Quaternion A
+ * \param b Quaternion B
+ * \return A * B
+ */
 Quaternion Quaternion_Multiply(const Quaternion a, const Quaternion b);
+
+/**
+ * Creates a unit quaternion
+ * \param a Quaternion A
+ * \return Unit quaternion
+ */
 Quaternion Quaternion_Unit(const Quaternion a);
-Vector     Quaternion_RotateVector(const Vector v, const Quaternion q);
+
+/**
+ * Rotates vector according to a quaternion
+ * \param v Vector to rotate
+ * \param q Rotation quaternion
+ * \return Rotated vector
+ */
+Vector Quaternion_RotateVector(const Vector v, const Quaternion q);
 
 #endif
